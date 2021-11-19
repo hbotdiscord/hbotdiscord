24d6611b818a5530fd0dead0ab9a01070fd2db57
NIGGERS <hbot-dsicord@bk.ru>
2021-11-19 14:19:18 +0700

    asdasdasdsa

---
+++ src/main/java/org/theootka/hbotdiscord/HbotdiscordApplication.java
@@ -0,0 +1,36 @@
+package org.theootka.hbotdiscord;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class HbotdiscordApplication {
+
+	@Autowired
+	private Environment env;
+	public static void main(String[] args) {
+		SpringApplication.run(HbotdiscordApplication.class, args);
+	}
+
+	@Bean
+	@ConfigurationProperties(value="discord-api")
+	public DiscordApi discordApi(){
+		String token = env.getProperty("TOKEN");
+		DiscordApi api = new DiscordApiBuilder().setToken(token)
+				.setAllNonPrivilegedIntents()
+				.login()
+				.join();
+		api.addMessageCreateListener(event->{
+			if(event.getMessageContent().equals("hb/ping")){
+				event.getChannel().sendMessage("pong");
+			}
+		});
+		return api;
+	}
+}
